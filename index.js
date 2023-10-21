960e63b839ebee939ca631bd331ace772b85f85e
MEZ901 <issammez44@gmail.com>
2023-10-21 16:07:17 +0100

    format document

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const Server = require("./src/infrastructure/webserver/Server");
 const { PORT } = require("./src/infrastructure/config/environment");
 const routes = require("./src/infrastructure/webserver/routes");
-const [
-  middlewares,
-  errMiddlewares,
-] = require("./src/infrastructure/webserver/middlewares");
+const [ middlewares, errMiddlewares] = require("./src/infrastructure/webserver/middlewares");
 
 new Server(PORT, routes, middlewares, errMiddlewares).start();
